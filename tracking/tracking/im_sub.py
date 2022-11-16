9ca49a7a6eef2647ba3e69491b81dde0512aec01
Dilan-Wijesinghe <dilan.wijesinghe0@gmail.com>
2022-11-16 15:58:53 -0600

    Created Tracking package for basic CV related operations

---
+++ tracking/tracking/im_sub.py
@@ -0,0 +1,32 @@
+# Imports 
+import rclpy 
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+
+class ImageSubscriber(Node):
+    def __init__(self):
+        super().__init__('image_subscriber')
+
+        # Creating the Subscriber, which receives info from the video_frames topic
+        self.vid_sub = self.create_subscription(Image, 'video_frames', self.sub_callback, 10)
+        self.bridge = CvBridge()
+
+    def sub_callback(self, data):
+        self.get_logger().info("Receving video_frames", once=True)
+        curr_frame = self.bridge.imgmsg_to_cv2(data) # Convert ROS image msg to OpenCV image
+        cv2.imshow("camera", curr_frame) # Display Image
+        cv2.waitKey(1)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ImageSubscriber()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+
