3f9f510ac287e42e92db84a6ce8d628647ac360c
Dilan-Wijesinghe <dilan.wijesinghe0@gmail.com>
2022-11-18 17:06:11 -0600

    Trying out several Object detection strategies. Laid the groundwork and implemented a few, but haven't finished yet.

--- tracking/tracking/im_sub.py
+++ tracking/tracking/im_sub.py
@@ -6,6 +6,66 @@ from cv_bridge import CvBridge
 import cv2 as cv
 import numpy as np
 
+max_value = 360//2
+low_h = 100
+high_h = 360//2
+low_s = 0
+high_s = 255
+low_v = 0
+high_v = 255
+wind_name = 'Balloon Color Tracking'
+
+def low_h_trackbar(val):
+    global low_h
+    global high_h
+    low_h = val
+    low_h = min(high_h - 1, low_h)
+    cv.setTrackbarPos('Low H', wind_name, low_h)
+
+def high_h_trackbar(val):
+    global low_h
+    global high_h
+    high_h = val
+    high_h = max(high_h, low_h + 1)
+    cv.setTrackbarPos('High H', wind_name, high_h)
+
+def low_s_trackbar(val):
+    global low_s
+    global high_s
+    low_s = val
+    low_s = min(high_s-1, low_s)
+    cv.setTrackbarPos('Low S', wind_name, low_s)
+
+def high_s_trackbar(val):
+    global low_s
+    global high_s
+    high_s = val
+    high_s = max(high_s, low_s+1)
+    cv.setTrackbarPos('High S', wind_name, high_s)
+
+def low_v_trackbar(val):
+    global low_v
+    global high_v
+    low_v = val
+    low_v = min(high_v-1, low_v)
+    cv.setTrackbarPos('Low V', wind_name, low_v)
+
+def high_v_trackbar(val):
+    global low_v
+    global high_v
+    high_v = val
+    high_v = max(high_v, low_v+1)
+    cv.setTrackbarPos('High V', wind_name, high_v)
+
+cv.namedWindow(wind_name )
+cv.createTrackbar('Low H', wind_name , low_h, 360//2, low_h_trackbar)
+cv.createTrackbar('High H', wind_name , high_h, 360//2, high_h_trackbar)
+cv.createTrackbar('Low S', wind_name , low_s, 360//2, low_s_trackbar)
+cv.createTrackbar('High S', wind_name , high_s, max_value, high_s_trackbar)
+cv.createTrackbar('Low V', wind_name , low_v, max_value, low_v_trackbar)
+cv.createTrackbar('High V', wind_name , high_v, max_value, high_v_trackbar)
+## [trackbar]
+
 # Get the number of cameras available
 def count_cameras():
     max_tested = 100
@@ -60,34 +120,82 @@ class ImageSubscriber(Node):
 
     def sub_callback(self, data):
         self.get_logger().info("Receiving video_frames", once=True)
-        curr_frame = self.bridge.imgmsg_to_cv2(data, desired_encoding='mono8') # Convert ROS image msg to OpenCV image
-        # frame_HSV = cv.cvtColor(curr_frame, cv.COLOR_BGR2HSV)
-        # frame_thresh = cv.inRange(frame_HSV (low_h, low_s, low_v), (high_h, high_s, high_v)
+        # curr_frame = self.bridge.imgmsg_to_cv2(data, desired_encoding='mono8') # Convert ROS image msg to OpenCV image
+        curr_frame = self.bridge.imgmsg_to_cv2(data, desired_encoding='bgr8') # Convert ROS image msg to OpenCV image
+        # gray = cv.cvtColor(curr_frame, cv.COLOR_BGR2GRAY)
+        frame_HSV = cv.cvtColor(curr_frame, cv.COLOR_BGR2HSV)
+        frame_thresh = cv.inRange(frame_HSV, (low_h, low_s, low_v), (high_h, high_s, high_v))
+        cv.imshow(wind_name, frame_thresh)
+
+        # Contours
+        contours, hier = cv.findContours(frame_thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+        # Find Canny Edges
+        # edged = cv.Canny(curr_frame)
+        # contours, hier = cv.findContours(edged, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+        # cv.imshow('Canny Edges', edged)
+
+        # cv.drawContours(curr_frame, )
+
+        # --- Centroids and Areas ---
+        # centroids = []
+        # areas = []
+        # for cont in contours:
+        #     currMom = cv.moments(cont)
+        #     cont_area = cv.contourArea(cont)
+        #     try: 
+        #         cx = int(currMom['m10']/currMom['m00'])
+        #         cy = int(currMom['m01']/currMom['m00'])
+        #         centroids.append([cx, cy])
+        #         areas.append(cont_area)
+
+        #     except:
+        #         pass
+        
+        # try: 
+        #     # Find Largest Contour
+        #     MaxCont = np.argmax(areas)
+        #     MaxCentroid = centroids[MaxCont]
+        #     curr_frame = cv.drawContours(curr_frame, contours, -1, (255,204,0))
+        #     curr_frame = cv.circle(curr_frame, MaxCentroid, 10, (24, 146, 221))
+        #     print(f"Centroid: {MaxCentroid}")
+        # except:
+        #     print("Contour not found")
+
+        # Find Threshold Range
+            # Sample multiple right pixels (from HSV)
+        # Denoise
+            # Get rid of chatter and outliers
+        # Depth Camera
+            # Get rid of something that is very far away 
+            # Background Subtraction is similar 
 
         # Background Subtraction
         # im_rgb = cv2.cvtColor(curr_frame, cv2.COLOR_RGB2BGR)
-        fgmask = self.bgsub.apply(curr_frame)
-        fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, self.kernel)
+        # fgmask = self.bgsub.apply(curr_frame)
+        # fgmask = cv.morphologyETransform
+        # param1 - sensitivityx(fgmask, cv.MORPH_OPEN, self.kernel)
 
-        # Hough Circle Transform
-        circles = cv.HoughCircles(curr_frame, cv.HOUGH_GRADIENT, 4, 200,
-                                  param1=100, param2=200, minRadius=80,maxRadius=200)
+        # Hough Circle 
+        # param2 - number of edgepoints
+        # circles = cv.HoughCircles(curr_frame, cv.HOUGH_GRADIENT, 1, 1000,
+        #                           param1=150, param2=60, minRadius=0, maxRadius=150)
         
-        if circles is None:
-            print("No circles!")
-            pass
-        else:
-            print(circles)
-            circles = np.uint16(np.around(circles))
+        # if circles is None:
+        #     print("No circles!")
+        #     pass
+        # else:
+        #     print(circles)
+        #     circles = np.uint16(np.around(circles))
 
-            for i in circles[0,:]:
-                cv.circle(curr_frame, (i[0], i[1]), i[2], (0,255,0), 2)
-                cv.circle(curr_frame, (i[0], i[1]), 2, (0, 0, 255), 3)
+        #     for i in circles[0,:]:
+        #         cv.circle(curr_frame, (i[0], i[1]), i[2], (0,255,0), 2)
+        #         cv.circle(curr_frame, (i[0], i[1]), 2, (0, 0, 255), 3)
         
         self.vid_pub.publish(self.bridge.cv2_to_imgmsg(curr_frame)) # Publish msg 
         # self.get_logger().info(self.bridge.cv_to_imgmsg(im_rgb), once=True)
 
-        cv.imshow("Camera", curr_frame) # Display image
+        cv.imshow("Camera + Contours", frame_HSV) # Display image
         # cv.imshow("FG Mask", fgmask) # Display foreground mask
         cv.waitKey(1)
     
