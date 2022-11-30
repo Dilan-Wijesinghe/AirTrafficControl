1f7c95ec6b1cb4338f1cc60a6e969a1dcd5858e1
muye1202 <muyejia2023@u.northwestern.edu>
2022-11-29 21:00:44 -0600

    Add in state machine.

--- motion_planning/motion_planning/hit.py
+++ motion_planning/motion_planning/hit.py
@@ -15,7 +15,7 @@ class State(Enum):
     """
 
     NOTSET = auto()
-    PREPARE = auto()
+    GO = auto()
     IN_HIT = auto()
 
 
@@ -32,6 +32,7 @@ class hit(Node):
         self.balloon_pos_x = []
         self.balloon_pos_y = []
         self.balloon_pos_z = []
+        self.state = State.NOTSET
         self.balloon_pos = Point()
         self.move_to = Pose()
 
@@ -66,75 +67,80 @@ class hit(Node):
             self.balloon_pos_z.append(b_pos[2])
             self.balloon_pos_x.append(b_pos[0])
             self.balloon_pos_y.append(b_pos[1])
-
-        x = self.balloon_pos_x
-        y = self.balloon_pos_y
-        z = self.balloon_pos_z
-        # fit a line in x-y plane
-        def xy_line(x, k, b):
-            return k*x + b
-
-        xy_param, _ = curve_fit(xy_line, x, y)
-
-        # fit a parabola in y-z plane
-        def yz_parabola(y, a, b, c):
-            return a*y**2 + b*y + c
-
-        parabola_param, _ = curve_fit(yz_parabola, y, z)
-
-        # When the balloon is within certain distance
-        # position the robot for finer hit pose
-        # give a default z coordinate
-        pred_z = 0.2
-        parabola_a = parabola_param[0]
-        parabola_b = parabola_param[1]
-        parabola_c = parabola_param[2] - pred_z
-
-        del_eqn = math.sqrt(parabola_b**2 - 4*parabola_a*parabola_c)
-        y1 = (-parabola_b + del_eqn)/(2*parabola_a)
-        y2 = (-parabola_b - del_eqn)/(2*parabola_a)
-
-        line_k = xy_param[0]
-        line_b = xy_param[1]
-        x1 = (y1-line_b)/line_k
-        x2 = (y2-line_b)/line_k
-
-        # TODO: choose the x y that's further from the 
-        # initial position of the balloon
-        init_x = self.balloon_pos_x[0]
-        init_y = self.balloon_pos_y[0]
-        dist_1 = math.sqrt((x1-init_x)**2 + (y1-init_y)**2)
-        dist_2 = math.sqrt((x2-init_x)**2 + (y2-init_y)**2)
-        if dist_1 < dist_2:
-            pred_x = x1
-            pred_y = y1
+        
         else:
-            pred_x = x2
-            pred_y = y2
+            self.state = State.GO
+        
+        if self.state == State.GO:
+            x = self.balloon_pos_x
+            y = self.balloon_pos_y
+            z = self.balloon_pos_z
+            # fit a line in x-y plane
+            def xy_line(x, k, b):
+                return k*x + b
 
-        # move the ee to this position
-        self.move_to.position.x = pred_x
-        self.move_to.position.y = pred_y
-        self.move_to.position.z = pred_z
+            xy_param, _ = curve_fit(xy_line, x, y)
 
-        # face the end effector upward
-        rot_ang = 0.0
-        x_ang = np.pi/2
-        y_ang = np.pi/2
-        z_ang = 0.0
+            # fit a parabola in y-z plane
+            def yz_parabola(y, a, b, c):
+                return a*y**2 + b*y + c
 
-        qw = np.cos(rot_ang/2)
-        qx = np.sin(rot_ang/2)*np.cos(x_ang)
-        qy = np.sin(rot_ang/2)*np.cos(y_ang)
-        qz = np.sin(rot_ang/2)*np.cos(z_ang)
+            parabola_param, _ = curve_fit(yz_parabola, y, z)
 
-        self.move_to.orientation.w = qw
-        self.move_to.orientation.x = qx
-        self.move_to.orientation.y = qy
-        self.move_to.orientation.z = qz
+            # When the balloon is within certain distance
+            # position the robot for finer hit pose
+            # give a default z coordinate
+            pred_z = 0.2
+            parabola_a = parabola_param[0]
+            parabola_b = parabola_param[1]
+            parabola_c = parabola_param[2] - pred_z
 
-        # publish this to Inverse Kinematics and move the arm
-        self.ee_pos_pub.publish(self.move_to)
+            del_eqn = math.sqrt(parabola_b**2 - 4*parabola_a*parabola_c)
+            y1 = (-parabola_b + del_eqn)/(2*parabola_a)
+            y2 = (-parabola_b - del_eqn)/(2*parabola_a)
+
+            line_k = xy_param[0]
+            line_b = xy_param[1]
+            x1 = (y1-line_b)/line_k
+            x2 = (y2-line_b)/line_k
+
+            # TODO: choose the x y that's further from the 
+            # initial position of the balloon
+            init_x = self.balloon_pos_x[0]
+            init_y = self.balloon_pos_y[0]
+            dist_1 = math.sqrt((x1-init_x)**2 + (y1-init_y)**2)
+            dist_2 = math.sqrt((x2-init_x)**2 + (y2-init_y)**2)
+            if dist_1 < dist_2:
+                pred_x = x1
+                pred_y = y1
+            else:
+                pred_x = x2
+                pred_y = y2
+
+            # move the ee to this position
+            self.move_to.position.x = pred_x
+            self.move_to.position.y = pred_y
+            self.move_to.position.z = pred_z
+
+            # face the end effector upward
+            rot_ang = 0.0
+            x_ang = np.pi/2
+            y_ang = np.pi/2
+            z_ang = 0.0
+
+            qw = np.cos(rot_ang/2)
+            qx = np.sin(rot_ang/2)*np.cos(x_ang)
+            qy = np.sin(rot_ang/2)*np.cos(y_ang)
+            qz = np.sin(rot_ang/2)*np.cos(z_ang)
+
+            self.move_to.orientation.w = qw
+            self.move_to.orientation.x = qx
+            self.move_to.orientation.y = qy
+            self.move_to.orientation.z = qz
+
+            # publish this to Inverse Kinematics and move the arm
+            self.ee_pos_pub.publish(self.move_to)
+            self.state = State.NOTSET
 
         # TODO: Generalize the ee pose
 
