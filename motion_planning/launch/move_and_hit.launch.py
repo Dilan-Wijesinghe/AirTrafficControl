8e299da1c1a90eff3dc15a445d03dd941302bb64
Rintaroh Shima <rintarohshima2023@u.northwestern.edu>
2022-12-08 01:59:23 -0600

    Created a launch file to launch simple_move and hit nodes

---
+++ motion_planning/launch/move_and_hit.launch.py
@@ -0,0 +1,25 @@
+import os
+
+from ament_index_python.packages import get_package_share_path
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(get_package_share_directory('motion_planning')), \
+                    '/simple_move.launch.py'])
+        ),
+        Node(
+            package='motion_planning',
+            executable='hit',
+            name='hit'
+        )
+    ])
